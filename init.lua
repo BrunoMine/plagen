e6421a9de09006eb6e3b490c8e4f6e16d25ecd0f
BrunoMine <borgesdossantosbruno@gmail.com>
2016-07-14 13:42:13 -0300

    Lançamento inicial

---
+++ init.lua
@@ -0,0 +1,22 @@
+--
+-- Mod PlaGen
+--
+-- Inicializador de scripts Lua
+--
+
+-- Notificador de Inicializador
+local notificar = function(msg)
+	if minetest.setting_get("log_mods") then
+		minetest.debug("[PLAGEN]"..msg)
+	end
+end
+
+local modpath = minetest.get_modpath("plagen")
+
+-- Variavel global
+plagen = {}
+
+-- Carregar scripts
+notificar("Carregando scripts...")
+dofile(modpath.."/planificador.lua")
+notificar("OK")
